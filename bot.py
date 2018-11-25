640c564e1b1113366351bc4410063a33c5cbefc1
Phantom <32412665+RobsPlayz@users.noreply.github.com>
2018-11-25 20:38:04 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,18 +21,7 @@ logging.basicConfig(level='INFO')
 newUserMessage = "A New user has joined!" # customise this to the message you want to send new users
 
 
-bot = commands.Bot(command_prefix="n!", description="help")
-
-@bot.command()
-async def mute(ctx,):
-   role = discord.utils.get(ctx.guild.roles, name="Muted")
-   user = ctx.message.author
-   await user.add_roles(role)
-@bot.command()
-async def unmute(ctx,):
-   role = discord.utils.get(ctx.guild.roles, name="Muted")
-   user = ctx.message.author
-   await user.remove_roles(role)
+bot = commands.Bot(command_prefix="!", description="help")
         
 @bot.command()
 @commands.has_permissions(ban_members=True)
@@ -47,15 +36,12 @@ async def kick(ctx, member: discord.Member):
                 
 @bot.command()
 async def credits(ctx,):
-    await ctx.send('Created by Drifty#6185 Helped by Bugless#1005 (Owner of Fortnite Drop)')
-@bot.command()
-async def invite(ctx,):
-    await ctx.send('https://discordapp.com/oauth2/authorize?&client_id=513156006973538313&scope=bot&permissions=268575766')
+    await ctx.send('Created by @Drifty#6185')
 @bot.command()
 async def send(ctx, *sendit):
     count = 0
     if not "PRMS" in [x.name for x in ctx.author.roles]:
-       await ctx.send('This command is not for you. If you are the owner, Do n!setup and give you the role PRMS.')
+       await ctx.send('This command is not for you. If you are the owner, Do !setup and give you the role PRMS.')
        return
     users = [x.id for x in ctx.guild.members]
     for x in users:
